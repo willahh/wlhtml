4ee50736cea79e89808a77a09eb88c5663aa3bbc
William Ravel <wravel@gmail.com>
2018-03-07 01:53:50 +0100

    Update

--- main.lisp
+++ main.lisp
@@ -5,8 +5,6 @@
 (use-package :cl-mysql)
 (setq db (connect :host "localhost" :user "root" :password "root" :database "bdd"))
 
-
-
 (defun get-all-from-table ()
   (query "SELECT * FROM `table`" :database db))
 
@@ -18,3 +16,14 @@
 
 (get-query-fields (get-all-from-table))
 (get-query-body (get-all-from-table))
+
+;; Connection avec clsql-sqlite3 en cours
+(ql:quickload "clsql-sqlite3")
+(use-package :clsql-sqlite3)
+
+(defvar *db* (sqlite3:sqlite3-open "/Users/willahh/www/projects/wlhtml/databases/wlhtml.db"))
+
+;; (clsql-sqlite3:sqlite3-database:)
+
+;; todo ------->
+(clsql-sqlite3::database-query "SELECT * FROM `table`" *db* :auto :auto)
